a4b9c73847b5a1313a3f86727f08a799253daf6c
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-03-01 14:07:49 -0600

    Add unity files

---
+++ Assets/Tortuga.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tortuga : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+    Vector3 vertical;
+    [SerializeField] float speed;
+    [SerializeField] float maxHeight;
+    [SerializeField] float minHeight;
+    // Update is called once per frame
+    void Update() {
+        vertical.y = Input.GetAxisRaw("Vertical");
+        Vector3 newPos = transform.position + vertical*speed;
+        transform.position = newPos;
+    }
+}
