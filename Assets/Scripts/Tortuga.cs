a50aff7444b3408c0bcf0d75a556d83222776723
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-03-02 17:34:02 -0600

    Add comments and order scripts

--- Assets/Scripts/Tortuga.cs
+++ Assets/Scripts/Tortuga.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+/*
+Controla el movimiento vertical de la tortuga. Cuando colisiona con otros
+objetos, cambia de tamaño momentáneamente.
+
+Lalo Villalpando
+02/03/2021
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tortuga : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
     Vector3 vertical;
     [SerializeField] float speed;
     [SerializeField] float maxHeight;
     [SerializeField] float minHeight;
+
     // Update is called once per frame
     void Update() {
         vertical.y = Input.GetAxisRaw("Vertical");
@@ -20,8 +25,10 @@ public class Tortuga : MonoBehaviour {
 
     public IEnumerator OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.tag == "Medusa"){
-            transform.localScale = new Vector3(0.65f, 0.65f, 1);            
+            // encojer a tortuga
+	        transform.localScale = new Vector3(0.65f, 0.65f, 1);            
         } else if (other.gameObject.tag == "Meta"){
+	        // hacer más grande a la tortuga
             transform.localScale = new Vector3(1.3f, 1.3f, 1);
         }
         yield return new WaitForSeconds(0.5f);
