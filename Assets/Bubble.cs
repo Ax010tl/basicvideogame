ab210410b51fc58c1c1676444a8dd8c57ac53ae9
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-03-01 18:00:39 -0600

    add stuff

---
+++ Assets/Bubble.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField] float maxHeight;
+    [SerializeField] float minHeight;
+    [SerializeField] float maxWidth;
+    [SerializeField] float minWidth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // Cuando se toque la burbuja, generar una nueva :D
+    public void OnTriggerEnter2D(Collider2D other){
+        Vector3 newPos;
+        newPos.x = Random.Range(minWidth, maxWidth);
+        newPos.y = Random.Range(minHeight, maxHeight);
+        newPos.z = 0;
+        transform.position = newPos;
+        print("Count++");
+    }
+}
---
+++ Assets/bubbleAdder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bubbleAdder : MonoBehaviour
+{
+    [SerializeField] GameObject newBubble;
+    [SerializeField] float maxHeight;
+    [SerializeField] float minHeight;
+    [SerializeField] float maxWidth;
+    [SerializeField] float minWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("addBubble", 0f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void addBubble(){
+        // Crear nuevo vector
+        Vector3 newPos;
+        newPos.x = Random.Range(minWidth, maxWidth);
+        newPos.y = Random.Range(minHeight, maxHeight);
+        newPos.z = 0;
+        // Crear objeto
+        GameObject newObject = Instantiate(newBubble, newPos, Quaternion.identity);
+        // Esperar n segundos
+        
+        Destroy(newObject, 10);
+    }
+}
